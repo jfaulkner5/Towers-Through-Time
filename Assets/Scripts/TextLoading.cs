34813e9263136417ac80655a8767ffd8a9813e8e
Dylan Thomson <dthomson3@sae.au>
2018-03-28 10:26:28 +1000

    made gamemanager, made ascii maps work with levels

--- Assets/Scripts/TextLoading.cs
+++ Assets/Scripts/TextLoading.cs
@@ -5,7 +5,6 @@ using System.IO;
 
 public class TextLoading : MonoBehaviour
 {
-
     string loadedFile = string.Empty;
 
     public Vector3 camStartPos;
@@ -13,6 +12,7 @@ public class TextLoading : MonoBehaviour
     public float sideLength;
     Vector3 newPos = Vector3.zero;
     public Camera cam;
+    public GameObject player;
 
     private void Start()
     {
@@ -22,12 +22,11 @@ public class TextLoading : MonoBehaviour
 
     void ReadFile()
     {
-        string path = "Assets/Resources/file.txt";
+        string path = Path.Combine(Application.dataPath, "LevelASCIIMaps\\Level" + GameManager.instance.currentLevel + ".txt");
 
         //Read the text from directly from the file.txt file
         StreamReader reader = new StreamReader(path);
         loadedFile = reader.ReadToEnd();
-        Debug.Log(loadedFile);
         reader.Close();
     }
 
@@ -35,20 +34,41 @@ public class TextLoading : MonoBehaviour
     {
         if (loadedFile != string.Empty)
         {
-            for (int index = 0; index < loadedFile.Length; index++)
+            switch (loadedFile[0])
+            {
+                case '1':
+                    SetVisuals(0);
+                    break;
+                case '2':
+                    SetVisuals(1);
+                    break;
+                case '3':
+                    SetVisuals(2);
+                    break;
+            }
+
+            for (int index = 2; index < loadedFile.Length; index++)
             {
                 newPos += new Vector3(sideLength, 0, 0);
                 switch (loadedFile[index])
                 {
+                    //new line of objects
                     case '\n':
                         newPos = new Vector3(0, 0, newPos.z + sideLength);
                         break;
+                    //base object
                     case '|':
-                        Instantiate(objectsToLoad[0],newPos,transform.rotation);
+                        Instantiate(objectsToLoad[0], newPos, transform.rotation);
                         break;
+                    //path object
                     case '+':
-                        Instantiate(objectsToLoad[1],newPos,transform.rotation);
+                        Instantiate(objectsToLoad[1], newPos, transform.rotation);
                         break;
+                    case 'P':
+                        Vector3 playerPos = new Vector3(newPos.x, newPos.y + 1, newPos.z);
+                        Instantiate(player, playerPos, transform.rotation);
+                        break;
+                    //empty space
                     case ' ':
                         break;
                 }
@@ -56,5 +76,10 @@ public class TextLoading : MonoBehaviour
         }
         cam.transform.position = camStartPos;
         cam.transform.eulerAngles = new Vector3(90, 0, 0);
+    }
+
+    void SetVisuals(int visualStyle)
+    {
+
     }
 }
