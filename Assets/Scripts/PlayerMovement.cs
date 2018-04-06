d9ff8ef2f4313d8fc138534e498fa3b0ff0458b2
JLinards <33106240+JLinards@users.noreply.github.com>
2018-04-06 15:56:32 +1000

    Changed the player movement script to get rid of the "slidy" feel

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,16 +8,36 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float inputHorizontal = Input.GetAxis("Horizontal");
-        float inputVertical = Input.GetAxis("Vertical");
+       // float inputHorizontal = Input.GetAxis("Horizontal");
+       // float inputVertical = Input.GetAxis("Vertical");
 
-        Vector3 moveDir = new Vector3(inputHorizontal, 0, inputVertical);
+        //Vector3 moveDir = new Vector3(inputHorizontal, 0, inputVertical);
 
-        transform.position += moveDir * Time.deltaTime * speed;
+        //transform.position += moveDir * Time.deltaTime * speed;
 
-        if(inputHorizontal != 0 || inputVertical != 0)
+        //if(inputHorizontal != 0 || inputVertical != 0)
+        //{
+        //    EventCore.Instance.playerWalk.Invoke();
+       // }
+
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            EventCore.Instance.playerWalk.Invoke();
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
     }
 }
